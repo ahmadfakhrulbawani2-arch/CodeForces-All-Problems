deb3ae005686cfc7122256e2437194774e5c2cfa
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-15 17:39:33 +0700

    Added Nov 15, 2025

---
+++ CodeForces-Dump-30-Oct-to-6-Nov-2025/mahmoud-ehab-and-even-odd-games.cpp
@@ -0,0 +1,26 @@
+// the explanation is in the bottom
+#include <iostream>
+
+int main(void) {
+    int n;
+    std::cin >> n;
+    if(n % 2 == 0) std::cout << "Mahmoud" << std::endl;
+    else std::cout << "Ehab" << std::endl;
+    return 0;
+}
+
+/*
+
+Link: https://codeforces.com/problemset/problem/959/A
+
+Repo: https://github.com/ahmadfakhrulbawani2-arch/CodeForces-Dump-22-to-29-Oct-2025
+
+Sometimes I forgot to change the link. Sorry
+*/
+
+/*
+=== EXPLANATION ===
+
+.
+
+*/
