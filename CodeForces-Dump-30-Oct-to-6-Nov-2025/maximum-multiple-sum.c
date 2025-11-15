9daab6237358ae0bd76165abbdb082a2cf0d91fb
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-15 17:39:37 +0700

    Added Nov 15, 2025

---
+++ CodeForces-Dump-30-Oct-to-6-Nov-2025/maximum-multiple-sum.c
@@ -0,0 +1,31 @@
+// the explanation is in the bottom
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        if(n == 2 || n == 3) printf("%d\n", n);
+        else if(n > 3) printf("2\n");
+    }
+    return 0;
+}
+
+
+/*
+
+Link: https://codeforces.com/problemset/problem/1985/A
+
+Repo: https://github.com/ahmadfakhrulbawani2-arch/CodeForces-Dump-30-Oct-to-6-Nov-2025 
+
+Sometimes I forgot to change the link. Sorry
+*/
+
+/*
+=== EXPLANATION ===
+
+.
+
+*/
