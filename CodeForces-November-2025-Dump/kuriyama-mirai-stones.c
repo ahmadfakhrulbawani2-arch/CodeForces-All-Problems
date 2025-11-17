fa993d4ff3258ce546f32ca7c2aad48dc5e778e9
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-17 22:46:05 +0700

    Added Nov 17, 2025

---
+++ CodeForces-November-2025-Dump/kuriyama-mirai-stones.c
@@ -0,0 +1,28 @@
+// بِسْمِ ٱللّٰهِ ٱلرَّحْمٰنِ ٱلرَّحِيمِ
+// اللَّهُمَّ صَلَّ عَلَى سَيِّدِنَا مُحَمَّدٍ.
+// the explanation is in the bottom
+
+#include <stdio.h>
+
+#define ll long long
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    return 0;
+}
+
+
+/*
+
+Link: https://codeforces.com/problemset/problem/433/B
+
+Repo: https://github.com/ahmadfakhrulbawani2-arch/CodeForces-All-Problems
+Sometimes I forgot to change the link. Sorry
+*/
+
+/*
+=== EXPLANATION ===
+
+
+*/
