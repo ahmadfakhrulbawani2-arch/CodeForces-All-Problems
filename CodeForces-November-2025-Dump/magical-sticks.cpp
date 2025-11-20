586c149805eeb1f8921633e3b986c48662b26053
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-21 00:02:33 +0700

    Added Nov 21, 2025

---
+++ CodeForces-November-2025-Dump/magical-sticks.cpp
@@ -0,0 +1,36 @@
+// بِسْمِ ٱللّٰهِ ٱلرَّحْمٰنِ ٱلرَّحِيمِ
+// اللَّهُمَّ صَلَّ عَلَى سَيِّدِنَا مُحَمَّدٍ.
+// the explanation is in the bottom
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        long long n;
+        cin >> n;
+        cout << (n + 1) / 2 << "\n";
+    }
+}
+
+
+
+/*
+
+Link: https://codeforces.com/problemset/problem/1371/A
+
+Repo: https://github.com/ahmadfakhrulbawani2-arch/CodeForces-All-Problems
+Sometimes I forgot to change the link. Sorry
+*/
+
+/*
+=== EXPLANATION ===
+
+
+*/
