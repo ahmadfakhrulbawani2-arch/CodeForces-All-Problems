611668be6d844ba10b5af38d0d431de3413ea600
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-22 23:02:06 +0700

    Added Nov 22, 2025

---
+++ CodeForces-November-2025-Dump/good-matrix-elements.c
@@ -0,0 +1,32 @@
+// بِسْمِ ٱللّٰهِ ٱلرَّحْمٰنِ ٱلرَّحِيمِ
+// اللَّهُمَّ صَلَّ عَلَى سَيِّدِنَا مُحَمَّدٍ.
+// the explanation is in the bottom
+
+#include <stdio.h>
+
+#define ll long long
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    int *arr = (int*) malloc(n*n * sizeof(int));
+    for(int i = 0; i < n; i++) {
+        for(int )
+    }
+    return 0;
+}
+
+
+/*
+
+Link: https://codeforces.com/problemset/problem/276/C
+
+Repo: https://github.com/ahmadfakhrulbawani2-arch/CodeForces-All-Problems
+Sometimes I forgot to change the link. Sorry
+*/
+
+/*
+=== EXPLANATION ===
+
+
+*/
